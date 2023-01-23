f116e9d94cf7a7d6494180506d51ead105f0c933
anaisprojetcours <anaisprojetcours@yourdomain.com>
2023-01-23 14:49:35 +0100

    add emoji

---
+++ 1_🏠Homepage.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+
+st.set_page_config(
+    page_title="SoScool", 
+    page_icon=":)"
+)
+
+st.title("Bienvenue dans l'application S'cool ")
+st.sidebar.success("select a page above")
+st.subheader('Ici vous trouverez la bonne école pour vos enfants facilement.')
+st.markdown(":smile:")
