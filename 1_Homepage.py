f8f60dad6eff011b8d5d37d3ba1a2b722bfe647d
anaisprojetcours <anaisprojetcours@yourdomain.com>
2023-01-23 17:00:00 +0100

    erroeyy

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -1,18 +1,8 @@
 import streamlit as st 
-import requests
-from streamlit_lottie import st_lottie
+
 
 st.set_page_config(page_title="SoScool", page_icon=":green_heart:")
 
 st.title("Bienvenue dans l'application S'cool ")
 st.sidebar.success("select a page above")
 st.subheader('Ici vous trouverez la bonne école pour vos enfants facilement.')
-
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-lottie_coding = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_jfrjiyor.json")
-st_lottie(lottie_coding, height=400, key="coding")
