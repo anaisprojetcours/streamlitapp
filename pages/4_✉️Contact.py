1bd2939657f8e0665a80ae407e32015a6d911299
anaisprojetcours <anaisprojetcours@yourdomain.com>
2023-01-23 15:46:08 +0100

    add contact form

--- pages/4_✉️Contact.py
+++ pages/4_✉️Contact.py
@@ -1,22 +1,29 @@
 import streamlit as st 
 
 st.title("Vous souhaitez nous contacter? ")
-st.subheader("Plusieurs options sont possibles! ")
+st.subheader("Plusieurs options sont possibles!😄 ")
 
 
-###Create a contact form
-contact_form= """"
-<form action="https://formsubmit.co/anaisprojetcours@gmail.com" method="POST">
-     <input type="text" name="name" placeholder="Votre nom" required>
-     <input type="email" name="email" placeholder="Votre email" required>
-     <textarea name="message" placeholder"Votre message"></textarea>
+###Create a contact fo
+
+
+
+contact_form = """
+<form action="https://formsubmit.co/YOUREMAIL@EMAIL.COM" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
      <button type="submit">Send</button>
 </form>
 """
+
 st.markdown(contact_form, unsafe_allow_html=True)
 
-def local_css(file_name): 
-    with open (file_name) as f:
+# Use Local CSS File
+def local_css(file_name):
+    with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
 
 local_css("./style/style.css")
