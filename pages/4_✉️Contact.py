c1ced5b294400d4bd333151b1a61bcce1c28d276
anaisprojetcours <anaisprojetcours@yourdomain.com>
2023-01-23 15:12:30 +0100

    create a conatct form

--- pages/4_✉️Contact.py
+++ pages/4_✉️Contact.py
@@ -2,3 +2,14 @@ import streamlit as st
 
 st.title("Vous souhaitez nous contacter? ")
 st.subheader("Plusieurs options sont possibles! ")
+
+
+###Create a contact form
+contact_form= """"
+<form action="https://formsubmit.co/anaisprojetcours@email.com" method="POST">
+     <input type="text" name="name" required>
+     <input type="email" name="email" required>
+     <button type="submit">Send</button>
+</form>
+"""
+st.markdown(contact_form, unsafe_allow_html=True)
